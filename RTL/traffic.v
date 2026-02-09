813ea413c6de4575c4b226c353f2ac88c18fac4f
eric710315 <115869509+eric710315@users.noreply.github.com>
2026-02-09 15:16:26 +0900

    Fix : start 버튼 추가, 실제 신호등 신호로 변경
    실제 신호등처럼 빨강 1000, 주황 0100, 좌회전 1010, 초록 0001로 할당. 보행자 신호는 clk=1일 때 초록(01), clk=0일 때 none(00)으로 깜빡임 구현

--- RTL/traffic.v
+++ RTL/traffic.v
@@ -21,93 +21,136 @@
 
 
 module traffic(
-    input clk, reset_n,
-    output reg [2:0] o_h_car_traffic, o_h_walker_traffic, o_v_car_traffic, o_v_walker_traffic
+    input clk, 
+    input reset_n,
+    input start,
+    output reg [3:0] o_h_car_traffic, 
+    output reg [3:0] o_v_car_traffic,
+    output reg [1:0] o_h_walker_traffic,    
+    output reg [1:0] o_v_walker_traffic
     );
     
-    parameter [2:0] RED = 3'b000;
-    parameter [2:0] GREEN = 3'b001;
-    parameter [2:0] YELLOW = 3'b010;
-    parameter [2:0] LEFT = 3'b011;
-    parameter [2:0] GREEN_TWINKLE = 3'b100;
+    parameter [3:0] C_RED = 4'b1000;
+    parameter [3:0] C_YELLOW = 4'b0100;
+    parameter [3:0] C_LEFT = 4'b0010;
+    parameter [3:0] C_GREEN = 4'b0001;
+    parameter [3:0] C_NONE = 4'b0000;
+    parameter [1:0] W_RED = 2'b10;
+    parameter [1:0] W_GREEN = 2'b01;
+    parameter [1:0] W_NONE = 2'b00;
+    
+    
     
     reg [6:0] r_cycle = 7'd0;
     
+    
     always@(posedge clk) begin
-        if (r_cycle == 7'd68 || reset_n == 1'b0) begin
-            r_cycle <= 7'd1;
+        if (start) begin
+            if (r_cycle == 7'd68 || reset_n == 1'b0) begin
+                r_cycle <= 7'd1;
+            end
+            else begin 
+                r_cycle <= r_cycle + 7'd1;
+            end
         end
-        else begin 
-            r_cycle <= r_cycle + 7'd1;
+        else begin
+            r_cycle <= 7'd0;
         end
     end
-    //첫 번째 주기에서 cycle은 초기 값을 가지고 이미 계산을 함. 첫 번째 주기의 cycle은 1이 됨.
-    //68 번째 주기 다음 주기에서 cycle은 1이 됨. 그러나 reg에는 68번째 주기의 값들이 들어감.
-    //reset도 마찬가지로 reset=0인 주기에서  cycle은 1이 되지만, reg는 이전의 cycle 값으로 판단한다. 
     
     always@(*) begin
-        if (r_cycle <= 7'd20) begin 
-            o_h_car_traffic = GREEN;
-        end
-        else if (r_cycle <= 7'd22) begin 
-            o_h_car_traffic = YELLOW;
-        end
-        else if (r_cycle <= 7'd32) begin 
-            o_h_car_traffic = LEFT;
-        end
-        else if (r_cycle <= 7'd34) begin 
-            o_h_car_traffic = YELLOW;
-        end
-        else begin 
-            o_h_car_traffic = RED;
-        end
+        if (start) begin
+            if (r_cycle <= 7'd20) begin 
+                o_h_car_traffic = C_GREEN;
+            end
+            else if (r_cycle <= 7'd22) begin 
+                o_h_car_traffic = C_YELLOW;
+            end
+            else if (r_cycle <= 7'd32) begin 
+                o_h_car_traffic = C_LEFT;
+            end
+            else if (r_cycle <= 7'd34) begin 
+                o_h_car_traffic = C_YELLOW;
+            end
+            else begin 
+                o_h_car_traffic = C_RED;
+            end
+         end
+         else begin
+            o_h_car_traffic = C_NONE;
+         end
      end
     
     always@(*) begin
-        if (r_cycle <= 7'd34) begin
-            o_v_car_traffic = RED;
-        end
-        else if (r_cycle <= 7'd54) begin
-            o_v_car_traffic = GREEN;
-        end
-        else if (r_cycle <= 7'd56) begin
-            o_v_car_traffic = YELLOW;
-        end
-        else if (r_cycle <= 7'd66) begin
-            o_v_car_traffic = LEFT;
-        end
-        else if (r_cycle <= 7'd68) begin
-            o_v_car_traffic = YELLOW;
+        if (start) begin
+            if (r_cycle <= 7'd34) begin
+                o_v_car_traffic = C_RED;
+            end
+            else if (r_cycle <= 7'd54) begin
+                o_v_car_traffic = C_GREEN;
+            end
+            else if (r_cycle <= 7'd56) begin
+                o_v_car_traffic = C_YELLOW;
+            end
+            else if (r_cycle <= 7'd66) begin
+                o_v_car_traffic = C_LEFT;
+            end
+            else if (r_cycle <= 7'd68) begin
+                o_v_car_traffic = C_YELLOW;
+            end
+            else begin
+                o_v_car_traffic = C_RED;
+            end
         end
         else begin
-            o_v_car_traffic = RED;
+            o_v_car_traffic = C_NONE;
         end
     end
     
     always@(*) begin
-        if (r_cycle <= 7'd34) begin
-            o_h_walker_traffic = RED;
-        end
-        else if (r_cycle <= 7'd48) begin
-            o_h_walker_traffic = GREEN;
-        end
-        else if (r_cycle <= 7'd54) begin
-            o_h_walker_traffic = GREEN_TWINKLE;
+        if (start) begin
+            if (r_cycle <= 7'd34) begin
+                o_h_walker_traffic = W_RED;
+            end
+            else if (r_cycle <= 7'd48) begin
+                o_h_walker_traffic = W_GREEN;
+            end
+            else if (r_cycle <= 7'd54) begin
+                if (clk) begin
+                    o_h_walker_traffic = W_GREEN;
+                end
+                else begin
+                    o_h_walker_traffic = W_NONE;
+                end
+            end
+            else begin
+                o_h_walker_traffic = W_RED;
+            end
         end
         else begin
-            o_h_walker_traffic = RED;
+            o_h_walker_traffic = W_NONE;
         end
     end
     
     always@(*) begin
-        if (r_cycle <= 7'd14) begin
-            o_v_walker_traffic = GREEN;
-        end
-        else if (r_cycle <= 7'd20) begin
-            o_v_walker_traffic = GREEN_TWINKLE;
+        if (start) begin
+            if (r_cycle <= 7'd14) begin
+                o_v_walker_traffic = W_GREEN;
+            end
+            else if (r_cycle <= 7'd20) begin
+                if (clk) begin
+                    o_v_walker_traffic = W_GREEN;
+                end
+                else begin
+                    o_v_walker_traffic = W_NONE;
+                end
+            end
+            else begin
+                o_v_walker_traffic = W_RED;
+            end
         end
         else begin
-            o_v_walker_traffic = RED;
+            o_v_walker_traffic = W_NONE;
         end
     end
     
