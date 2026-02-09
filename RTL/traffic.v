266e720d643f2dee78ebd4fc09253db8dd4a8012
eric710315 <115869509+eric710315@users.noreply.github.com>
2026-02-09 23:12:21 +0900

    Feat : i_flag, o_flag 추가
    4개의 모듈만 가지고 사거리 신호 만들 수 있도록

--- RTL/traffic.v
+++ RTL/traffic.v
@@ -24,8 +24,10 @@ module traffic(
     input wire clk, 
     input wire reset_n,
     input wire i_start,
+    input wire i_flag,
     output reg [3:0] o_car_traffic, 
-    output reg [1:0] o_walker_traffic
+    output reg [1:0] o_walker_traffic,
+    output reg o_flag
     );
     
     parameter [3:0] C_RED = 4'b1000;
@@ -38,18 +40,22 @@ module traffic(
     parameter [1:0] W_NONE = 2'b00;
     
     
-    
     reg [6:0] r_cycle;
     
+    always@(*) begin
+        o_flag = ~i_flag;
+    end
     
     always@(posedge clk) begin
         if (!reset_n) begin
             r_cycle <= 7'd0;
+            o_flag <= 1'b0;
         end
         else begin
             if (i_start) begin
-                if (r_cycle == 7'd68) begin
+                if (r_cycle == 7'd34) begin
                     r_cycle <= 7'd1;
+                    o_flag <= ~o_flag;
                 end
                 else begin 
                     r_cycle <= r_cycle + 7'd1;
@@ -63,45 +69,54 @@ module traffic(
     
     always@(*) begin
         if (i_start) begin
-            if (r_cycle <= 7'd20) begin 
-                o_car_traffic = C_GREEN;
-            end
-            else if (r_cycle <= 7'd22) begin 
-                o_car_traffic = C_YELLOW;
-            end
-            else if (r_cycle <= 7'd32) begin 
-                o_car_traffic = C_LEFT;
-            end
-            else if (r_cycle <= 7'd34) begin 
-                o_car_traffic = C_YELLOW;
-            end
-            else begin 
+            if (i_flag) begin
+                if (r_cycle <= 7'd20) begin 
+                    o_car_traffic = C_GREEN;
+                end
+                else if (r_cycle <= 7'd22) begin 
+                    o_car_traffic = C_YELLOW;
+                end
+                else if (r_cycle <= 7'd32) begin 
+                    o_car_traffic = C_LEFT;
+                end
+                else if (r_cycle <= 7'd34) begin 
+                    o_car_traffic = C_YELLOW;
+                end
+                else begin 
+                    o_car_traffic = C_NONE;
+                end
+             end
+             else begin
                 o_car_traffic = C_RED;
-            end
+             end
          end
+         
          else begin
             o_car_traffic = C_NONE;
          end
+         
      end
     
     always@(*) begin
         if (i_start) begin
-            if (r_cycle <= 7'd34) begin
+            if (i_flag) begin            
                 o_walker_traffic = W_RED;
-            end
-            else if (r_cycle <= 7'd48) begin
-                o_walker_traffic = W_GREEN;
-            end
-            else if (r_cycle <= 7'd54) begin
-                if (r_cycle[0] == 1'b0) begin
-                    o_walker_traffic = W_GREEN;
-                end
-                else begin
-                    o_walker_traffic = W_NONE;
-                end
             end
             else begin
-                o_walker_traffic = W_RED;
+                if (r_cycle <= 7'd14) begin
+                    o_walker_traffic = W_GREEN;
+                end
+                else if (r_cycle <= 7'd20) begin
+                    if (r_cycle[0] == 1'b0) begin
+                        o_walker_traffic = W_GREEN;
+                    end
+                    else begin
+                        o_walker_traffic = W_NONE;
+                    end
+                end
+                else begin
+                    o_walker_traffic = W_RED;
+                end
             end
         end
         else begin
