5281920066dabb8911cea19b92a58a424d557e26
eric710315 <115869509+eric710315@users.noreply.github.com>
2026-02-11 14:15:38 +0900

    Fix : 다른 always문에서 중복으로 값 할당, state 값
    서로 다른 always문에서 중복으로 바뀌는 값들 없앰. state를 상태로 정하기. 원래는 신호 값으로 줬음

--- RTL/traffic_fsm.v
+++ RTL/traffic_fsm.v
@@ -31,17 +31,22 @@ module traffic_fsm(
 
     );
     
-    parameter [3:0] C_RED       = 4'b1000;
+    parameter [3:0] C_GREEN     = 4'b0001;
     parameter [3:0] C_YELLOW    = 4'b0100;
     parameter [3:0] C_LEFT      = 4'b0010;
-    parameter [3:0] C_GREEN     = 4'b0001;
-    parameter [3:0] C_NONE      = 4'b0000;
+    parameter [3:0] C_RED       = 4'b1000;
+//    parameter [3:0] C_NONE      = 4'b0000;
     parameter [1:0] W_RED       = 2'b10;
     parameter [1:0] W_GREEN     = 2'b01;
     parameter [1:0] W_NONE      = 2'b00;
     
-    reg [3:0] c_state;
-    reg [3:0] c_next;
+    parameter [1:0] S0 = 2'b00;     //cG    wR 
+    parameter [1:0] S1 = 2'b01;     //cY    wG
+    parameter [1:0] S2 = 2'b10;     //cL    wN
+    parameter [1:0] S3 = 2'b11;     //cR
+    
+    reg [1:0] c_state;
+    reg [1:0] c_next;
     
     reg [1:0] w_state;
     reg [1:0] w_next;
@@ -53,21 +58,19 @@ module traffic_fsm(
     
     always@(*) begin
         case (c_state) 
-            C_GREEN     : c_next = C_YELLOW;
-            C_YELLOW    : c_next = (r_c_sel ? C_LEFT : C_RED);
-            C_LEFT      : c_next = C_YELLOW;
-            C_YELLOW    : c_next = C_RED;
-            C_RED       : c_next = C_GREEN;
-            default     : c_next = C_NONE;
+            S0      : c_next = S1;
+            S1      : c_next = (r_c_sel ? S2 : S0);
+            S2      : c_next = S1;
+            S3      : c_next = S0;
         endcase
     end
     
     always@(*) begin
         case (w_state)
-            W_GREEN     : w_next = (r_w_sel ? W_NONE : W_RED);
-            W_NONE      : w_next = (r_w_sel ? W_GREEN : W_RED);
-            W_RED       : w_next = W_GREEN;
-            default     : w_next = W_NONE;
+            S0      : w_next = S1;
+            S1      : w_next = (r_w_sel ? S2 : S0);
+            S2      : w_next = (r_w_sel ? S1 : S0);
+            default : w_next = S2;
         endcase
     end
             
@@ -75,15 +78,13 @@ module traffic_fsm(
         if (!reset_n) begin
             if (i_flag) begin
                 r_cycle <= 7'd0;
-                c_state <= C_RED;
+                r_c_sel <= 1'b1;
                 r_w_sel <= 1'b0;
-                w_state <= W_NONE;
             end
             else begin
                 r_cycle <= 7'd34;
                 r_c_sel <= 1'b0;
-                c_state <= C_YELLOW;
-                w_state <= W_RED;
+                r_w_sel <= 1'b1;
             end
         end
         else begin
@@ -102,7 +103,13 @@ module traffic_fsm(
             
     always@(posedge clk) begin
         if (i_start) begin
-            if (r_cycle == 7'd0 || r_cycle == 7'd20 || r_cycle == 7'd22 || r_cycle == 7'd32 || r_cycle == 7'd34 || r_cycle == 7'd68) begin
+            if (r_cycle == 7'd0) begin
+                c_state <= S0;
+            end
+            else if (r_cycle == 7'd34) begin
+                c_state <= S3;
+            end
+            else if (r_cycle == 7'd20 || r_cycle == 7'd22 || r_cycle == 7'd32 || r_cycle == 7'd68) begin
                 c_state <= c_next;
                 if (r_cycle == 7'd20) begin
                     r_c_sel <= 1'b1;
@@ -117,13 +124,18 @@ module traffic_fsm(
             end
         end
         else begin
-            o_car_traffic <= C_NONE;
         end
     end
     
     always@(posedge clk) begin
         if (i_start) begin
-            if (r_cycle == 7'd0 || r_cycle == 7'd34 || r_cycle == 7'd48 || r_cycle == 7'd49 || r_cycle == 7'd50 || r_cycle == 7'd51 || r_cycle == 7'd52 || r_cycle == 7'd53 || r_cycle == 7'd54) begin
+            if (r_cycle == 7'd0) begin
+                w_state <= S0;
+            end
+            else if (r_cycle == 7'd34) begin
+                w_state <= S1;
+            end
+            else if (r_cycle == 7'd48 || r_cycle == 7'd49 || r_cycle == 7'd50 || r_cycle == 7'd51 || r_cycle == 7'd52 || r_cycle == 7'd53 || r_cycle == 7'd54) begin
                 w_state <= w_next;
                 if (r_cycle == 7'd53) begin
                     r_w_sel <= 1'b0;
@@ -137,13 +149,24 @@ module traffic_fsm(
             end
         end
         else begin
-            o_walker_traffic <= W_NONE;
         end
     end
     
     always@(*) begin
-        o_car_traffic = c_state;
-        o_walker_traffic = w_state;
+        case (c_state)
+            S0 : o_car_traffic = C_GREEN;
+            S1 : o_car_traffic = C_YELLOW;
+            S2 : o_car_traffic = C_LEFT;
+            S3 : o_car_traffic = C_RED;
+        endcase
+    end
+    always@(*) begin
+        case (w_state)
+            S0      : o_walker_traffic = W_RED;
+            S1      : o_walker_traffic = W_GREEN;
+            S2      : o_walker_traffic = W_NONE;
+            default : o_walker_traffic = W_NONE;
+        endcase
     end
     
 endmodule
