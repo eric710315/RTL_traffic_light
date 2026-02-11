19086abf0bdfc0f06692ec94f5096c3774a45bf7
eric710315 <115869509+eric710315@users.noreply.github.com>
2026-02-11 15:55:20 +0900

    Fix : none 상태 추가, top 모듈 추가

--- RTL/traffic_fsm.v
+++ RTL/traffic_fsm.v
@@ -18,7 +18,48 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
+module top_fsm(
+    input wire clk,
+    input wire reset_n,
+    input wire i_start,
+    output wire [3:0] o_e_ct,
+    output wire [3:0] o_w_ct,
+    output wire [3:0] o_s_ct,
+    output wire [3:0] o_n_ct,
+    output wire [1:0] o_e_wt,
+    output wire [1:0] o_w_wt,
+    output wire [1:0] o_s_wt,
+    output wire [1:0] o_n_wt
+    );
+    traffic_fsm U0 (.clk(clk),
+                .reset_n(reset_n),
+                .i_start(i_start),
+                .i_flag(1'b0),
+                .o_car_traffic(o_e_ct),
+                .o_walker_traffic(o_e_wt));
+        
+    traffic_fsm U1 (.clk(clk),
+                .reset_n(reset_n), 
+                .i_start(i_start), 
+                .i_flag(1'b1), 
+                .o_car_traffic(o_w_ct), 
+                .o_walker_traffic(o_w_wt));
+        
+    traffic_fsm U2 (.clk(clk), 
+                .reset_n(reset_n), 
+                .i_start(i_start), 
+                .i_flag(1'b0), 
+                .o_car_traffic(o_s_ct), 
+                .o_walker_traffic(o_s_wt));
+        
+    traffic_fsm U3 (.clk(clk), 
+                .reset_n(reset_n), 
+                .i_start(i_start), 
+                .i_flag(1'b1), 
+                .o_car_traffic(o_n_ct), 
+                .o_walker_traffic(o_n_wt));
+    
+endmodule
 
 module traffic_fsm(
 
@@ -40,16 +81,20 @@ module traffic_fsm(
     parameter [1:0] W_GREEN     = 2'b01;
     parameter [1:0] W_NONE      = 2'b00;
     
-    parameter [1:0] S0 = 2'b00;     //cG    wR 
-    parameter [1:0] S1 = 2'b01;     //cY    wG
-    parameter [1:0] S2 = 2'b10;     //cL    wN
-    parameter [1:0] S3 = 2'b11;     //cR
+    parameter [2:0] SCG = 3'b000;     //cG   
+    parameter [2:0] SCY = 3'b001;     //cY    
+    parameter [2:0] SCL = 3'b010;     //cL    
+    parameter [2:0] SCR = 3'b011;     //cR
+    parameter [2:0] SCN = 3'b100;
+    parameter [2:0] SWR = 3'b101;
+    parameter [2:0] SWG = 3'b110;
+    parameter [2:0] SWN = 3'b111;
     
-    reg [1:0] c_state;
-    reg [1:0] c_next;
+    reg [2:0] c_state;
+    reg [2:0] c_next;
     
-    reg [1:0] w_state;
-    reg [1:0] w_next;
+    reg [2:0] w_state;
+    reg [2:0] w_next;
     
     reg r_c_sel;
     reg r_w_sel;
@@ -58,35 +103,34 @@ module traffic_fsm(
     
     always@(*) begin
         case (c_state) 
-            S0      : c_next = S1;
-            S1      : c_next = (r_c_sel ? S2 : S3);
-            S2      : c_next = S1;
-            S3      : c_next = S0;
-            default : c_next = S3;
+            SCG     : c_next = SCY;
+            SCY     : c_next = (r_c_sel ? SCL : SCR);
+            SCL     : c_next = SCY;
+            SCR     : c_next = SCG;
+            SCN     : c_next = (r_c_sel ? SCG : SCR);
+            default : c_next = SCR;
         endcase
         case (w_state)
-            S0      : w_next = S1;
-            S1      : w_next = (r_w_sel ? S2 : S0);
-            S2      : w_next = S1;
-            default : w_next = S2;
+            SWR     : w_next = SWG;
+            SWG     : w_next = (r_w_sel ? SWN : SWR);
+            SWN     : w_next = (r_w_sel ? SWG : SWR);
+            default : w_next = SWR;
         endcase
     end
             
     always@(posedge clk) begin
         if (!reset_n) begin
+            c_state <= SCN;
+            w_state <= SWN;
             if (i_flag) begin
                 r_cycle <= 7'd0;
                 r_c_sel <= 1'b1;
-                r_w_sel <= 1'b1;
-                c_state <= S3;
-                w_state <= S0;
+                r_w_sel <= 1'b0;
             end
             else begin
                 r_cycle <= 7'd34;
                 r_c_sel <= 1'b0;
                 r_w_sel <= 1'b1;
-                c_state <= S1;
-                w_state <= S0;
             end
         end
         else begin
@@ -97,11 +141,12 @@ module traffic_fsm(
                 end
                 else begin
                     r_cycle <= r_cycle + 7'd1;
-                    if (r_cycle == 7'd34) begin
+                    if (r_cycle == 7'd0 || r_cycle == 7'd34) begin
                         c_state <= c_next;
                         w_state <= w_next;
+                        r_w_sel <= 1'b1;
                     end
-                    else if (r_cycle == 7'd0 || r_cycle == 7'd20 || r_cycle == 7'd22 || r_cycle == 7'd32) begin
+                    else if (r_cycle == 7'd20 || r_cycle == 7'd22 || r_cycle == 7'd32) begin
                         c_state <= c_next;
                         if (r_cycle == 7'd20) begin
                             r_c_sel <= 1'b1;
@@ -131,25 +176,20 @@ module traffic_fsm(
     end
  
     always@(*) begin
-        if (!reset_n || !i_start) begin
-            o_car_traffic = C_NONE;
-            o_walker_traffic = W_NONE;
-        end
-        else begin
-            case (c_state)
-                S0 : o_car_traffic = C_GREEN;
-                S1 : o_car_traffic = C_YELLOW;
-                S2 : o_car_traffic = C_LEFT;
-                S3 : o_car_traffic = C_RED;
-                default : o_car_traffic = C_NONE;
-            endcase
-            case (w_state)
-                S0      : o_walker_traffic = W_RED;
-                S1      : o_walker_traffic = W_GREEN;
-                S2      : o_walker_traffic = W_NONE;
-                default : o_walker_traffic = W_NONE;
-            endcase
-        end
+        case (c_state)
+            SCG : o_car_traffic = C_GREEN;
+            SCY : o_car_traffic = C_YELLOW;
+            SCL : o_car_traffic = C_LEFT;
+            SCR : o_car_traffic = C_RED;
+            SCN : o_car_traffic = C_NONE;
+            default : o_car_traffic = C_NONE;
+        endcase
+        case (w_state)
+            SWR     : o_walker_traffic = W_RED;
+            SWG     : o_walker_traffic = W_GREEN;
+            SWN     : o_walker_traffic = W_NONE;
+            default : o_walker_traffic = W_NONE;
+        endcase
     end
     
 endmodule
