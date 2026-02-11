7b144cb843aa45bde98b5bf89bfbe3e7e1cd3717
eric710315 <115869509+eric710315@users.noreply.github.com>
2026-02-11 22:03:58 +0900

    Fix : combination logic에 latch 삭제
    combination logic은 else begin ~ end 안이 비어있으면 안됨. 
    combination logic은 값을 바로 반영하기 때문에 sequential logic에 들어가는 값이 바로 바뀌어버림.

--- RTL/traffic_fsm.v
+++ RTL/traffic_fsm.v
@@ -124,42 +124,34 @@ module traffic_fsm(
         end
     end
     
-    always@(*) begin
-        if (!reset_n) begin
+    always@(*) begin    //combination logic은 값이 바뀔 때 바로 값 반영. sequential에 들어가는 값이 바로 바뀜.
+        if (!reset_n || !i_start) begin
             c_next = SCN;
             w_next = SWN;
-//            if (i_flag) begin
-//                c_next = SCG;
-//                w_next = SWR;
-//            end
-//            else begin
-//                c_next = SCR;
-//                w_next = SWG;
-//            end
         end
         
-        else begin
-            if (7'd0 <= r_cycle || r_cycle < 7'd20) begin
+        else begin  
+            if ((7'd0 <= r_cycle && r_cycle < 7'd20) || r_cycle == 7'd68) begin
+                c_next = SCG;
+                w_next = SWR;
+            end
+            else if (7'd20 <= r_cycle && r_cycle < 7'd22) begin
                 c_next = SCY;
                 w_next = SWR;
             end
-            else if (7'd20 <= r_cycle || r_cycle < 7'd22) begin
+            else if (7'd22 <= r_cycle && r_cycle < 7'd32) begin
                 c_next = SCL;
                 w_next = SWR;
             end
-            else if (7'd22 <= r_cycle || r_cycle < 7'd32) begin
+            else if  (7'd32 <= r_cycle && r_cycle < 7'd34) begin
                 c_next = SCY;
                 w_next = SWR;
             end
-            else if  (7'd32 <= r_cycle || r_cycle < 7'd34) begin
+            else if (7'd34 <= r_cycle && r_cycle < 7'd48) begin
                 c_next = SCR;
                 w_next = SWG;
             end
-            else if (7'd34 <= r_cycle || r_cycle < 7'd48) begin
-                c_next = SCR;
-                w_next = SWG;
-            end
-            else if (7'd48 <= r_cycle || r_cycle < 7'd54) begin
+            else if (7'd48 <= r_cycle && r_cycle < 7'd54) begin
                 c_next = SCR;
                 if (r_cycle[0] == 0) begin
                     w_next = SWN;
@@ -168,30 +160,13 @@ module traffic_fsm(
                     w_next = SWG;
                 end
             end
-            else if (7'd54 <= r_cycle || r_cycle < 7'd68) begin
+            else  begin
                 c_next = SCR;
                 w_next = SWR;
             end
         end
     end
-    
-//    always@(*) begin
-//        case (c_state) 
-//            SCG     : c_next = SCY;
-//            SCY     : c_next = (r_c_sel ? SCL : SCR);
-//            SCL     : c_next = SCY;
-//            SCR     : c_next = SCG;
-//            SCN     : c_next = (r_c_sel ? SCG : SCR);
-//            default : c_next = SCR;
-//        endcase
-//        case (w_state)
-//            SWR     : w_next = SWG;
-//            SWG     : w_next = (r_w_sel ? SWN : SWR);
-//            SWN     : w_next = (r_w_sel ? SWG : SWR);
-//            default : w_next = SWR;
-//        endcase
-//    end
-            
+ 
     always@(posedge clk) begin
         c_state <= c_next;
         w_state <= w_next;
